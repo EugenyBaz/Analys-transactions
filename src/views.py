a26659a8e23b3c2166e7b3449150ff99019acf37
Bazavod <eugeny.bazavod@list.ru>
2025-02-06 21:28:04 +0300

    add func for views greeting aв card_info

--- src/views.py
+++ src/views.py
@@ -3,6 +3,7 @@ import pandas as pd
 from typing import Any
 from datetime import datetime
 import math
+import json
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 project_root = os.path.abspath(os.path.join(current_dir, ".."))
@@ -11,7 +12,8 @@ data_file_path_exl = os.path.join(project_root, "data", "operations.xlsx")
 def read_transactions_exl(file_path: Any) -> list[dict[Any, Any]]:
     """Функция получения, чтения файла excel, преобразование в список словарей транзакций"""
     df = pd.read_excel(file_path)
-    list_transactions_exl = df.to_dict(orient="records")
+    df_filtered = df.dropna(subset=['Номер карты'])
+    list_transactions_exl = df_filtered.to_dict(orient="records")
     return list_transactions_exl
 # print (read_transactions_exl(data_file_path_exl))
 
@@ -21,9 +23,9 @@ def greeting(time_str):
     hour = dt.hour
 
     if  5 <= hour < 11:
-        return ("Доброе утро!")
+        return str(("Доброе утро!"))
     elif  11 <= hour < 18 :
-        return ("Добрый день!")
+        return str(("Добрый день!"))
     elif  18  <= hour < 23:
         return ("Добрый вечер!")
     else:
@@ -44,9 +46,42 @@ def card_number(transactions):
             else:
                 continue  # Пропускаем значения, которые не являются ни строковыми, ни числовыми
             result.append(f"{value[-4:]}")  # Берем последние 4 символа
-    return result
+    return set(result)
 transactions =  read_transactions_exl(data_file_path_exl)
 print (card_number(transactions))
+
+
+def card_info(transactions):
+    trans_list = {}
+    for trans in transactions:
+
+        card_number_f= trans['Номер карты']
+        card_number = card_number_f[1:5].replace(card_number_f[:-4], card_number_f[-4:])
+        amount = trans.get('Сумма операции', 0)
+
+        if card_number is not None:
+            round_amount = round(amount,2)
+
+            if card_number in trans_list:
+
+                total_sum = trans_list[card_number] + round_amount
+                trans_list[card_number] = round(total_sum, 2)
+            else:
+
+                trans_list[card_number] = round_amount
+
+    return trans_list
+
+print(card_info(transactions))
+
+final_result = {"greeting": greeting('2024-12-31 12:30:45'), "cards" :[card_info(transactions)]}
+
+ff_result = json.dumps(final_result, indent=4, ensure_ascii= False)
+with open('proba.json', 'w', encoding= 'utf-8') as f:
+    f.write(ff_result)
+
+
+
 
 
 
