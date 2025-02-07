f54ee7fe92ddc8aee9367945f1b3c654417366ba
Bazavod <eugeny.bazavod@list.ru>
2025-02-07 19:30:44 +0300

    add " top_transactions"

--- src/views.py
+++ src/views.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
 from typing import Any
-from datetime import datetime
+from datetime import datetime, date
 import math
 import json
 
@@ -10,18 +10,18 @@ project_root = os.path.abspath(os.path.join(current_dir, ".."))
 data_file_path_exl = os.path.join(project_root, "data", "operations.xlsx")
 
 def read_transactions_exl(file_path: Any) -> list[dict[Any, Any]]:
-    """Функция получения, чтения файла excel, преобразование в список словарей транзакций"""
+    """Функция получения, чтения файла excel, преобразование в список словарей без нулевых значений по номеру карты"""
     df = pd.read_excel(file_path)
     df_filtered = df.dropna(subset=['Номер карты'])
     list_transactions_exl = df_filtered.to_dict(orient="records")
     return list_transactions_exl
-# print (read_transactions_exl(data_file_path_exl))
 
-date_string = '2021-12-15'
-current_date = datetime.strptime(date_string,'%Y-%m-%d' )
-start_of_month = current_date.replace(day=1)
-start_date = start_of_month.strftime('%Y-%m-%d'),
-end_date = current_date.strftime('%Y-%m-%d')
+def read_transactions_exl_all(file_path: Any) -> list[dict[Any, Any]]:
+    """Функция получения, чтения файла excel, преобразование в список словарей  без нулевых значений по дате"""
+    df = pd.read_excel(file_path)
+    df_filtered = df.dropna(subset=['Дата платежа'])
+    list_transactions_exl_all = df_filtered.to_dict(orient="records")
+    return list_transactions_exl_all
 
 
 
@@ -38,9 +38,10 @@ def greeting(time_str):
         return ("Добрый вечер!")
     else:
         return ("Доброй ночи!")
+# date_time = input(" Введите дату и время в формате YYYY-MM-DD HH:MM:SS")
+date_time = '2021-12-31 12:30:45'
 
-
-print (greeting('2024-12-31 12:30:45'))
+print (greeting(date_time))
 
 def card_number(transactions):
     result = []
@@ -58,12 +59,10 @@ def card_number(transactions):
 transactions =  read_transactions_exl(data_file_path_exl)
 print (card_number(transactions))
 
-
-date_string = '15.12.2021'
-current_date = datetime.strptime(date_string,'%d.%m.%Y')
-start_of_month = current_date.replace(day=1)
+dt_obj = datetime.strptime(date_time, '%Y-%m-%d %H:%M:%S')
+start_of_month = dt_obj.replace(day=1)
 start_date = start_of_month.strftime('%d.%m.%Y')
-end_date = current_date.strftime('%d.%m.%Y')
+end_date = dt_obj.strftime('%d.%m.%Y')
 
 def card_info(transactions,start,end):
     result = []
@@ -82,42 +81,65 @@ def card_info(transactions,start,end):
             card_number = card_number_f[1:5].replace(card_number_f[:-4], card_number_f[-4:])
             amount = trans.get('Сумма операции', 0)
 
-            if card_number not in trans_list:
-                trans_list[card_number] = 0
+            if float(amount) < 0:
 
-            trans_list[card_number] += float(amount)
+                if card_number not in trans_list:
+                    trans_list[card_number] = 0
+
+                trans_list[card_number] += float(amount)
 
     for key,value in trans_list.items():
         result.append ({
         'last_digits': key,
-        'total_spent': round(value, 2),
-        'cashback': round((round(value, 2)/ 100), 2)
+        'total_spent': -round(value, 2),
+        'cashback': -round((round(value, 2)/ 100), 2)
     })
 
     return result
 
-print(card_info(transactions, start_date,end_date ))
+print(card_info(transactions, start_date,end_date))
 
-final_result = {"greeting": greeting('2024-12-31 12:30:45'), "cards" :[card_info(transactions, start_date,end_date )]}
+
+
+def sort_by_amount(transactions,start,end, reverse_str: bool = True):
+    """Функция сортировки по сумме операций по убыванию"""
+    result = []
+    filtered_transactions = []  # Список для хранения транзакций в пределах указанного диапазона
+
+    if isinstance(start, str):
+        start = datetime.strptime(start, '%d.%m.%Y').date()
+    if isinstance(end, str):
+        end = datetime.strptime(end, '%d.%m.%Y').date()
+
+    # Фильтруем транзакции по дате
+    for trans in transactions:
+        transaction_date: date = datetime.strptime(trans['Дата платежа'], '%d.%m.%Y').date()
+        if start <= transaction_date <= end:
+            filtered_transactions.append(trans)
+
+    # Сортируем отфильтрованные транзакции по сумме операции
+    sorted_transactions = sorted(filtered_transactions, key=lambda x: float(x["Сумма операции"]), reverse= reverse_str)
+
+    # Формируем результат, ограничивая до 5 записей
+    for i in range(min(len(sorted_transactions), 5)):
+        trans = sorted_transactions[i]
+        result.append({
+            "date": trans['Дата платежа'],
+            "amount": trans['Сумма операции'],
+            "category": trans['Категория'],
+            "description": trans['Описание']
+        })
+
+    return result
+transactions_all = read_transactions_exl_all(data_file_path_exl)
+
+print(sort_by_amount(transactions_all, start_date,end_date))
+
+
+
+final_result = {"greeting": greeting(date_time), "cards" :[card_info(transactions, start_date,end_date )],
+                "top_transactions": [sort_by_amount(transactions_all, start_date,end_date)]}
 
 ff_result = json.dumps(final_result, indent=4, ensure_ascii= False)
 with open('proba.json', 'w', encoding= 'utf-8') as f:
     f.write(ff_result)
-
-
-
-
-
-
-# def sort_by_date(
-#     list_dict: List[Dict[str, Union[str, int]]], reverse_str: bool = True
-# ) -> List[Dict[str, Union[str, int]]]:
-#     """Функция сортировки по дате по убыванию"""
-#     for i in list_dict:
-#         if "date" not in i or i["date"] == "":
-#             raise ValueError("Отсутствует дата")
-#
-#     sorted_list = sorted(list_dict, key=lambda x: x["date"], reverse=reverse_str)
-#
-#     return sorted_list
-
